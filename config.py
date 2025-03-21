095a789993f486e859a4dc4ebb88c808cc50b3d4
kay-ex <106707683+Kayxzy@users.noreply.github.com>
2025-03-21 19:55:55 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
-    API_ID="12323"
-    API_HASH="ksjs"
-    TOKEN="jaksks"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "5264285143").split(" "))
+    API_ID="17131033"
+    API_HASH="7768488c115ac09684bb38e608c47997"
+    TOKEN="7295562044:AAGXQD04WsgTHjC-Dp-MXmMtpJToIEL4EpI"
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "6886313636").split(" "))
     START_IMG="jdjsjw"
-    BOT_ID=1013
-    BOT_USERNAME="djsjw"
+    BOT_ID=7295562044
+    BOT_USERNAME="babunyaadt_bot"
     BOT_NAME="BAN ALL"
